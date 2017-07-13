923d77c30185d5a7307208f1feb90d76f793f3c0
huge <2252603873@qq.com>
2017-07-13 11:00:12 +0800

    增加关于、标签

--- lua/controller/tag.lua
+++ lua/controller/tag.lua
@@ -2,6 +2,7 @@ local cjson = require "cjson"
 local req = require "dispatch.req"
 local result = require "common.result"
 local tag_service = require "service.tag_service"
+local template = require("resty.template")
 
 local _M = {}
 
@@ -9,6 +10,14 @@ local _M = {}
 function _M:list()
 	local list = tag_service:list()
 	ngx.say(cjson.encode(result:success("查询成功", list)))
+end
+
+-- 标签云页面
+function _M:index()
+	local list = tag_service:list()
+	local context = {list = list }
+	
+	template.render("blog/tags.html", context)
 end
 
 return _M
